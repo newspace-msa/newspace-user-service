bf8fba7476be8401d79542162d5243ab63dcd970
hyun minyoung <kingmandoo95@gmail.com>
2025-03-28 14:36:08 +0900

    git init

---
+++ src/main/java/com/lgcns/newspaceuserservice/NewspaceUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.newspaceuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaceUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspaceUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgcns/newspaceuserservice/NewspaceUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.newspaceuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaceUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
